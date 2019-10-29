ed13e6a31d88cb99c5ee7b2c27d016bfb2155837
Matt Ward <iammatthewward@gmail.com>
2019-10-29 11:56:29 +0000

    feat(ftplugin): add files for css, json, terraform

--- install.sh
+++ install.sh
@@ -6,5 +6,8 @@ ln -sfv "$DOTFILES_DIR/.alias" ~
 ln -sfv "$DOTFILES_DIR/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/.vim/ftplugin/javascript.vim" ~/.vim/ftplugin/javascript.vim
 ln -sfv "$DOTFILES_DIR/.vim/ftplugin/elixir.vim" ~/.vim/ftplugin/elixir.vim
+ln -sfv "$DOTFILES_DIR/.vim/ftplugin/css.vim" ~/.vim/ftplugin/css.vim
+ln -sfv "$DOTFILES_DIR/.vim/ftplugin/json.vim" ~/.vim/ftplugin/json.vim
+ln -sfv "$DOTFILES_DIR/.vim/ftplugin/terraform.vim" ~/.vim/ftplugin/terraform.vim
 ln -sfv "$DOTFILES_DIR/.vimrc" ~
 ln -sfv "$DOTFILES_DIR/.zshrc" ~
