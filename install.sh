03523e6ca828131873cbd4c98f6b4c64db0fcc13
Matt Ward <iammatthewward@gmail.com>
2020-01-18 16:16:01 +0000

    feat(vim): add ftdetect for cjs files

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-mkdir -p ~/.vim/ftplugin
+mkdir -p ~/.vim/{ftplugin,ftdetect}
 ln -sfv "$DOTFILES_DIR/.alias" ~
 ln -sfv "$DOTFILES_DIR/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/.vim/ftdetect/cjs.vim" ~/.vim/ftdetect/cjs.vim
 ln -sfv "$DOTFILES_DIR/.vim/ftplugin/javascript.vim" ~/.vim/ftplugin/javascript.vim
 ln -sfv "$DOTFILES_DIR/.vim/ftplugin/elixir.vim" ~/.vim/ftplugin/elixir.vim
 ln -sfv "$DOTFILES_DIR/.vim/ftplugin/css.vim" ~/.vim/ftplugin/css.vim
